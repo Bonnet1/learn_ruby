3440890c79d4679e3df29752d401aa62cfb141e5
Bonnet1 <c.mannion84@gmail.com>
2019-03-04 18:31:04 -0500

    Completed exercises 00, 01, and 06

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc fahrenheit
+    fahrenheit = fahrenheit.to_f
+    (fahrenheit-32) * 5/9
+end
+
+def ctof celsius
+    celsius = celsius.to_f
+    32 + (celsius * 9/5)
+end
